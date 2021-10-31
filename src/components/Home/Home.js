1589e39fe5ef3258c42d56e169c021975e6c97b8
Faisalnk4045 <foysalnk9095@gmail.com>
2021-10-31 21:40:39 +0600

    testimonial sectiona added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Blogs from '../Blogs/Blogs';
+import InfoSection from '../InfoSection/InfoSection';
 import OurRooms from '../OurRooms/OurRooms';
+import Testimonial from '../Testimonial/Testimonial';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <OurRooms></OurRooms>
+            <Testimonial></Testimonial>
+            <InfoSection></InfoSection>
             <Blogs></Blogs>
         </div>
     );
---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+const InfoSection = () => {
+    return (
+        <div style={{ backgroundColor: '#222222' }} className='container-fluid py-5'>
+            <Row xs={1} lg={4} className="g-4 text-center">
+                <Col>
+                    <div>
+                        <h2 style={{ color: '#c5a880' }}><b>38,900</b></h2>
+                        <h6 style={{ color: '#c7c7c7' }}># of Happy Guests</h6>
+                    </div>
+                </Col>
+                <Col>
+                    <div>
+                        <h2 style={{ color: '#c5a880' }}><b>320</b></h2>
+                        <h6 style={{ color: '#c7c7c7' }}># of Rooms</h6>
+                    </div>
+                </Col>
+                <Col>
+                    <div>
+                        <h2 style={{ color: '#c5a880' }}><b>1,000</b></h2>
+                        <h6 style={{ color: '#c7c7c7' }}># of Staffs</h6>
+                    </div>
+                </Col>
+                <Col>
+                    <div>
+                        <h2 style={{ color: '#c5a880' }}><b>587</b></h2>
+                        <h6 style={{ color: '#c7c7c7' }}># of Destination</h6>
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default InfoSection;
